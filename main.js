a8d15329e0b3d064a85594c6157eb876102bca2d
B022110114 <b02110114@student.utem.edu.my>
2024-06-17 02:13:40 +0800

    update

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://b022110114:Wanasofea01@zawanah.yaxiom4.mongodb.net/?retryWrites=true&w=majority&appName=Zawanah"
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
    serverApi: {
       version: ServerApiVersion.v1,
@@ -18,7 +19,10 @@ const client = new MongoClient(uri, {
 });
 async function run() {
    try {
+      // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
+      // Send a ping to confirm a successful connection
+      //  await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
    } finally {
       // Ensures that the client will close when you finish/error
