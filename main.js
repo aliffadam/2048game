f1616b92580c49c94fc8a063e787cb0fcc727cdb
B022110114 <b02110114@student.utem.edu.my>
2024-06-22 00:31:11 +0800

    update

--- main.js
+++ main.js
@@ -32,6 +32,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
